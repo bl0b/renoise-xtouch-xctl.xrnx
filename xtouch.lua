3de4207c5fd97d356073479085103f44115cb44d
damien <damien.leroux@gmail.com>
2017-11-11 18:20:37 +0100

    Bidirectional communication begun.

--- xtouch.lua
+++ xtouch.lua
@@ -3,6 +3,44 @@
 
 class "XTouch"
 
+
+-- Match a color to one in the X-Touch
+-- X-Touch defines:
+-- 1 Red
+-- 2 Green
+-- 3 Yellow
+-- 4 Blue
+-- 5 Magenta
+-- 6 Cyan
+-- 7 White
+
+function match_color(r, g, b)
+  local xtcol = {{255, 0, 0}, {0, 255, 0}, {255, 255, 0}, {0, 0, 255}, {255, 0, 255}, {0, 255, 255}, {255, 255, 255}}
+  local best = 7
+  local bestproj = 1000000
+  if r == 0 and g == 0 and b == 0 then
+    return 0
+  end
+  for i = 1, #xtcol do
+    local dr = (xtcol[i][1] - r)
+    local dg = (xtcol[i][2] - g)
+    local db = (xtcol[i][3] - b)
+    dr = dr * dr
+    dg = dg * dg
+    db = db * db
+    local proj = math.sqrt(dr + dg + db)
+    if (proj < bestproj) then
+      bestproj = proj
+      best = i
+    end
+  end
+  return best
+end
+
+
+
+
+
 -- dump a midi message in the terminal
 -- (prefix, message) -> nil
 function print_msg(s, msg)
@@ -58,7 +96,7 @@ end
 -- (channel) -> (outfunc, screen table) -> nil
 function send_screen(channel)
   return function (out, screen)
-    local msg = {0xf0, 0, 0, 0x66, 0x58, 0x20 + channel, screen.color, 0,0,0,0,0,0,0, 0,0,0,0,0,0,0, 0xf7}
+    local msg = {0xf0, 0, 0, 0x66, 0x58, 0x20 + channel, match_color(screen.color[1], screen.color[2], screen.color[3]), 0,0,0,0,0,0,0, 0,0,0,0,0,0,0, 0xf7}
     str2arr(screen.lines[1], msg, 1)
     str2arr(screen.lines[2], msg, 2)
     out(msg)
@@ -72,6 +110,7 @@ function send_fader(channel)
   local pb = 0xe0 + channel
   return function(out, value)
     value = math.floor(16384 * value)
+    print("send fader ", value, bit.band(value, 0x7f), bit.rshift(value, 7))
     out({pb, bit.band(value, 0x7f), bit.rshift(value, 7)})
   end
 end
@@ -81,6 +120,7 @@ end
 -- (channel) -> (outfunc, value) -> nil
 function send_led(number)
   return function(out, value)
+    print("send led ", value)
     out({0x90, number, value})
   end
 end
@@ -109,7 +149,7 @@ local message_types = {
       send_track(0), send_track(1), send_track(2), send_track(3),
       send_track(4), send_track(5), send_track(6), send_track(7)
     },
-    main_fader = 0,
+    main_fader = send_fader(8),
     encoder_assign = {
       track = send_led(40),
       pan = send_led(42),
@@ -174,119 +214,273 @@ local message_types = {
     scrub = send_led(101),
   }
 
+local note_map = {
+-- 0
+ 'rec_1',
+ 'rec_2',
+ 'rec_3',
+ 'rec_4',
+ 'rec_5',
+-- 5
+ 'rec_6',
+ 'rec_7',
+ 'rec_8',
+ 'solo_1',
+ 'solo_2',
+-- 10
+ 'solo_3',
+ 'solo_4',
+ 'solo_5',
+ 'solo_6',
+ 'solo_7',
+-- 15
+ 'solo_8',
+ 'mute_1',
+ 'mute_2',
+ 'mute_3',
+ 'mute_4',
+-- 20
+ 'mute_5',
+ 'mute_6',
+ 'mute_7',
+ 'mute_8',
+ 'select_1',
+-- 25
+ 'select_2',
+ 'select_3',
+ 'select_4',
+ 'select_5',
+ 'select_6',
+-- 30
+ 'select_7',
+ 'select_8',
+ 'encoder_push_1',
+ 'encoder_push_2',
+ 'encoder_push_3',
+-- 35
+ 'encoder_push_4',
+ 'encoder_push_5',
+ 'encoder_push_6',
+ 'encoder_push_7',
+ 'encoder_push_8',
+-- 40
+ 'enc_assign_track',
+ 'enc_assign_send',
+ 'enc_assign_pan',
+ 'enc_assign_plugin',
+ 'enc_assign_eq',
+-- 45
+ 'enc_assign_inst',
+ 'fader_bank_left',
+ 'fader_bank_right',
+ 'channel_left',
+ 'channel_right',
+-- 50
+ 'flip',
+ 'global_view',
+ 'display',
+ 'smpte_beats',
+ 'f1',
+-- 55
+ 'f2',
+ 'f3',
+ 'f4',
+ 'f5',
+ 'f6',
+-- 60
+ 'f7',
+ 'f8',
+ 'midi_tracks',
+ 'inputs',
+ 'audio_tracks',
+-- 65
+ 'audio_inst',
+ 'aux',
+ 'buses',
+ 'outputs',
+ 'user',
+-- 70
+ 'shift',
+ 'option',
+ 'control',
+ 'alt',
+ 'read_off',
+-- 75
+ 'write',
+ 'trim',
+ 'touch',
+ 'latch',
+ 'group',
+-- 80
+ 'save',
+ 'undo',
+ 'cancel',
+ 'enter',
+ 'marker',
+-- 85
+ 'nudge',
+ 'cycle',
+ 'drop',
+ 'replace',
+ 'click',
+-- 90
+ 'solo',
+ 'transport_rewind',
+ 'transport_forward',
+ 'transport_stop',
+ 'transport_play',
+-- 95,
+ 'transport_record',
+ 'up',
+ 'down',
+ 'left',
+ 'right',
+-- 100
+ 'zoom',
+ 'scrub',
+ nil,
+ nil,
+ 'fader_touch_1',
+-- 105
+ 'fader_touch_2',
+ 'fader_touch_3',
+ 'fader_touch_4',
+ 'fader_touch_5',
+ 'fader_touch_6',
+-- 110
+ 'fader_touch_7',
+ 'fader_touch_8',
+ 'fader_toouch_9',
+ nil,
+ nil,
+-- 115
+ 'solo', 
+ nil, 
+ nil, 
+ nil,
+ nil,
+-- 120
+ nil, 
+ nil, 
+ nil, 
+ nil,
+ nil,
+-- 125
+ nil, 
+ nil, 
+ nil
+}
 
--- Match Renoise color to one in the X-Touch
--- X-Touch defines:
--- 1 Red
--- 2 Green
--- 3 Yellow
--- 4 Blue
--- 5 Magenta
--- 6 Cyan
--- 7 White
 
-function match_color(r, g, b)
-  local xtcol = {{255, 0, 0}, {0, 255, 0}, {255, 255, 0}, {0, 0, 255}, {255, 0, 255}, {0, 255, 255}, {255, 255, 255}}
-  local best = 7
-  local bestproj = 1000000
-  for i = 1, #xtcol do
-    local dr = (xtcol[i][1] - r)
-    local dg = (xtcol[i][2] - g)
-    local db = (xtcol[i][3] - b)
-    dr = dr * dr
-    dg = dg * dg
-    db = db * db
-    local proj = math.sqrt(dr + dg + db)
-    if (proj < bestproj) then
-      bestproj = proj
-      best = i
+-- Find what settings change and how to send these changes by simple pattern matching
+-- (out, new_partial_status, current_status, message_methods) -> nil (side effects: messages sent and current_status is updated)
+
+function find_changes(out, new_partial_status, current_status, message_methods)
+  print("Entering find_changes…")
+  rprint(new_partial_status)
+  for k, v in pairs(new_partial_status) do
+    print("On key ", k)
+    if type(message_methods[k]) == 'table' then
+      print("… is a table!")
+      rprint(v)
+      current_status[k] = current_status[k] or {}
+      find_changes(out, v, current_status[k], message_methods[k])
+    else
+      print("  value is ", v)
+      if v ~= current_status[k] then
+        print("Updating status…")
+        current_status[k] = v
+        message_methods[k](out, v)
+      end
     end
   end
-  return best
+  print("Exiting find_changes…")
 end
 
 
+-- Controller class Ctor
 function XTouch:__init(midiin, midiout)
   print("CTOR XTouch")
   oprint(midiin)
   self.input = renoise.Midi.create_input_device(midiin, function (msg) self:parse_msg(msg) end, function (msg) self:parse_msg(msg) end)
   self.output = renoise.Midi.create_output_device(midiout)
   self.output:send({0xf0, 0x00, 0x00, 0x66, 0x58, 0x01, 0x30, 0x31, 0x35, 0x36, 0x34, 0x30, 0x33, 0x35, 0x39, 0x32, 0x41, 0xf7})
-  self.status = {
+  self.status = {}
+  local status = {
     encoder_leds = {0, 0, 0, 0, 0, 0, 0, 0},
     tracks = {
-      {screen = {color = 0x41, lines = {'Hi', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.},
-      {screen = {color = 0x42, lines = {'Hello', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.},
-      {screen = {color = 0x43, lines = {'Hi', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.},
-      {screen = {color = 0x44, lines = {'Hello', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.},
-      {screen = {color = 0x45, lines = {'Hi', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.},
-      {screen = {color = 0x46, lines = {'Hello', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.},
-      {screen = {color = 0x47, lines = {'Hi', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.},
-      {screen = {color = 0x48, lines = {'Hello', 'Renoise'}}, rec = false, solo = false, mute = false, select = false, vu = 0., fader = 0.}
+      {screen = {color = {0, 255, 0}, lines = {'Hello', 'Renoise'}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.},
+      {screen = {color = {0, 255, 255}, lines = {'X-Touch', '(XCtl)'}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.},
+      {screen = {color = {255, 255, 255}, lines = {'Support', 'by bl0b'}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.},
+      {screen = {color = {0, 0, 0}, lines = {'', ''}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.},
+      {screen = {color = {0, 0, 0}, lines = {'', ''}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.},
+      {screen = {color = {0, 0, 0}, lines = {'', ''}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.},
+      {screen = {color = {0, 0, 0}, lines = {'', ''}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.},
+      {screen = {color = {0, 0, 0}, lines = {'', ''}}, rec = 0, solo = 0, mute = 0, select = 0, vu = 0., fader = 0.}
     },
-    main_fader = 0,
+    main_fader = .75,
     encoder_assign = {
-      track = false,
-      pan = false,
-      eq = false,
-      send = false,
-      plugin = false,
-      inst = false
+      track = 0,
+      pan = 0,
+      eq = 0,
+      send = 0,
+      plugin = 0,
+      inst = 0
     },
-    display_name_or_value = false,
-    global_view = false,
-    midi_tracks = false,
-    inputs = false,
-    audio_tracks = false,
-    aux = false,
-    buses = false,
-    outputs = false,
-    user = false,
-    flip = false,
-    f1 = false,
-    f2 = false,
-    f3 = false,
-    f4 = false,
-    f5 = false,
-    f6 = false,
-    f7 = false,
-    f8 = false,
-    shift = false,
-    option = false,
-    read_off = false,
-    write = false,
-    trim = false,
-    save = false,
-    undo = false,
-    control = false,
-    alt = false,
-    touch = false,
-    latch = false,
-    group = false,
-    cancel = false,
-    enter = false,
-    marker = false,
-    nudge = false,
-    cycle = false,
-    drop = false,
-    replace = false,
-    click = false,
-    solo = false,
-    transport_rewind = false,
-    transport_forward = false,
-    transport_stop = false,
-    transport_play = false,
-    transport_record = false,
-    bank_left = false,
-    bank_right = false,
-    channel_left = false,
-    channel_right = false,
-    left = false,
-    up = false,
-    down = false,
-    right = false,
-    zoom = false,
-    scrub = false,
+    display_name_or_value = 0,
+    global_view = 0,
+    midi_tracks = 0,
+    inputs = 0,
+    audio_tracks = 0,
+    aux = 0,
+    buses = 0,
+    outputs = 0,
+    user = 0,
+    flip = 0,
+    f1 = 0,
+    f2 = 0,
+    f3 = 0,
+    f4 = 0,
+    f5 = 0,
+    f6 = 0,
+    f7 = 0,
+    f8 = 0,
+    shift = 0,
+    option = 0,
+    read_off = 0,
+    write = 0,
+    trim = 0,
+    save = 0,
+    undo = 0,
+    control = 0,
+    alt = 0,
+    touch = 0,
+    latch = 0,
+    group = 0,
+    cancel = 0,
+    enter = 0,
+    marker = 0,
+    nudge = 0,
+    cycle = 0,
+    drop = 0,
+    replace = 0,
+    click = 0,
+    solo = 0,
+    transport_rewind = 0,
+    transport_forward = 0,
+    transport_stop = 0,
+    transport_play = 0,
+    transport_record = 0,
+    bank_left = 0,
+    bank_right = 0,
+    channel_left = 0,
+    channel_right = 0,
+    left = 0,
+    up = 0,
+    down = 0,
+    right = 0,
+    zoom = 0,
+    scrub = 0,
   }
   
   renoise.tool():add_timer(function() self:ping() end, 6000)
@@ -296,20 +490,21 @@ function XTouch:__init(midiin, midiout)
     print_msg('Sending', msg)
     self.output:send(msg)
   end
-  for i = 0, 7 do
-    local t = renoise.song().tracks[i + 1]
-    if t ~= nil then
-      self.status.tracks[i + 1].screen.color = match_color(t.color[1], t.color[2], t.color[3])
-    else
-      self.status.tracks[i + 1].screen.color = 0
-    end
-    send_screen(i)(out, self.status.tracks[i + 1].screen)
-    send_fader(i)(out, .75)
-    send_enc_led(i)(out, (i + 1) * 1024 - 1)
-    send_vu(i)(out, .15 * i)
---    send_vu2(i)(out, .15 * i)
-  end
-  send_led(50)(out, 1)
+  self:update(status)
+--  for i = 0, 7 do
+--    local t = renoise.song().tracks[i + 1]
+--    if t ~= nil then
+--      self.status.tracks[i + 1].screen.color = match_color(t.color[1], t.color[2], t.color[3])
+--    else
+--      self.status.tracks[i + 1].screen.color = 0
+--    end
+--    send_screen(i)(out, self.status.tracks[i + 1].screen)
+--    send_fader(i)(out, .75)
+--    send_enc_led(i)(out, (i + 1) * 1024 - 1)
+--    send_vu(i)(out, .15 * i)
+----    send_vu2(i)(out, .15 * i)
+--  end
+--  send_led(50)(out, 1)
 end
 
 
@@ -320,12 +515,35 @@ end
 
 
 function XTouch:parse_msg(msg)
-  print_msg('Received', msg)
+  local cmd = bit.rshift(msg[1], 4)
+  local chan = bit.band(msg[1], 0xF)
+  if cmd == 0x9 or cmd == 0x8 then
+    local label = note_map[msg[2] + 1]
+    local pressed = (cmd == 0x9 and msg[3] > 0)
+    print("cmd ", cmd, " vel ", msg[3], " pressed ", pressed)
+    if pressed then
+      print("A button was pressed! " .. label)
+    else
+      print("A button was released! " .. label)
+    end
+  elseif cmd == 0xB then
+    print("0xB")
+  elseif cmd == 0xC then
+    print("control change")
+  elseif cmd == 0xD then
+    print("0xD")
+  elseif cmd == 0xE then
+    print("0xE")
+  else
+    print_msg('Received', msg)
+  end
 end
 
 
 function XTouch:update(partial_status)
-  for k, v in pairs(partial_status) do
-    oprint(k, v)
+  local out = function(msg)
+    print_msg('Updating', msg)
+    self.output:send(msg)
   end
+  find_changes(out, partial_status, self.status, message_types)
 end
