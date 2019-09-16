234cf4586e2ebaf3e3589bc71256cfc8a29c89f6
Damien Leroux <damien.leroux@gmail.com>
2019-09-16 00:11:54 -0700

    Preparing for first release

--- make_xrnx.sh
+++ make_xrnx.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf build_dir
 mkdir build_dir
 cd build_dir
-git clone git@github.com:bl0b/renoise-xtouch-xctl.xrnx.git .
-zip -r ../dev.bl0b.X-Touch.xrnx *
+
+#git clone git@github.com:bl0b/renoise-xtouch-xctl.xrnx.git .
+git clone .. .
+
+mv manifest.xml.release manifest.xml
+
+VERSION=`grep '<Version' manifest.xml | sed 's/.*>\(.*\)<.*/\1/'`
+
+zip -r ../dev.bl0b.X-Touch_$VERSION.xrnx *
 cd ..
 rm -rf build_dir
