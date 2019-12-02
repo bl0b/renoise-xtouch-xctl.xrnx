a2afa557da51ab4d5b4c069ce343c0bd9444db69
Damien Leroux <damien.leroux@gmail.com>
2019-12-02 13:01:47 -0800

    changed jogwheel logic to work with lines instead of beats.

--- Programs/mixer_lib/transport.lua
+++ Programs/mixer_lib/transport.lua
@@ -24,38 +24,75 @@ function transport(xtouch, state)
       { xtouch = 'xtouch.transport.jog_wheel,delta',
         callback = function()
           local timestamp = os.clock()
-          local multiplier = 1.0 / renoise.song().transport.lpb
+          local multiplier = 1
           if last_jog_wheel_timestamp then
             local delta_t = timestamp - last_jog_wheel_timestamp
             if delta_t < 0.005 then
-              multiplier = 8
+              multiplier = renoise.song().transport.lpb * 8
             elseif delta_t < 0.010 then
-              multiplier = 4
+              multiplier = renoise.song().transport.lpb * 4
             elseif delta_t < 0.025 then
-              multiplier = 2
+              multiplier = renoise.song().transport.lpb * 2
             elseif delta_t < 0.05 then
-              multiplier = 1
+              multiplier = renoise.song().transport.lpb * 1
             end
             -- print("jog wheel multiplier", xtouch.transport.jog_wheel.delta.value, last_jog_wheel_timestamp, timestamp, multiplier)
           end
           last_jog_wheel_timestamp = timestamp
-          local len = renoise.song().transport.song_length_beats
-          local cur
-          if renoise.song().transport.playing then
-            cur = renoise.song().transport.playback_pos_beats
-          else
-            cur = renoise.song().transport.edit_pos_beats
+
+          local song = renoise.song()
+          local seq = song.sequencer
+          local transport = song.transport
+          local pos = transport.playing and transport.playback_pos or transport.edit_pos
+          local max = transport.song_length
+          pos.line = pos.line + (xtouch.transport.jog_wheel.delta.value * multiplier)
+          print('before', pos, max)
+          local p = song:pattern(seq:pattern(pos.sequence))
+          while pos.line > p.number_of_lines and pos.sequence < max.sequence do
+            pos.line = pos.line - p.number_of_lines
+            pos.sequence = pos.sequence + 1
+            p = song:pattern(seq:pattern(pos.sequence))
           end
-          cur = cur + (xtouch.transport.jog_wheel.delta.value * multiplier)
-          if cur < 0 then cur = 0 end
-          if cur >= len then cur = len - (1.0 / renoise.song().transport.lpb) end
-          pcall(function()
+          while pos.line < 1 and pos.sequence > 1 do
+            pos.sequence = pos.sequence - 1
+            p = song:pattern(seq:pattern(pos.sequence))
+            pos.line = pos.line + p.number_of_lines
+          end
+          if pos.sequence == 1 and pos.line < 1 then
+            pos.line = 1
+          end
+          if pos.sequence >= max.sequence and pos.line >= max.line then
+            pos.line = max.line
+          end
+
+          print('after', pos, max)
+
+          xpcall(function()
             if renoise.song().transport.playing then
-              renoise.song().transport.playback_pos_beats = cur
+              renoise.song().transport.playback_pos = pos
             else
-              renoise.song().transport.edit_pos_beats = cur
+              renoise.song().transport.edit_pos = pos
             end
-          end)
+          end, function(err) print(err) print(debug.traceback()) end)
+
+          -- local len = renoise.song().transport.lpb * renoise.song().transport.song_length_beats
+          -- local cur
+          -- if renoise.song().transport.playing then
+          --   cur = renoise.song().transport.lpb * renoise.song().transport.playback_pos_beats
+          -- else
+          --   cur = renoise.song().transport.lpb * renoise.song().transport.edit_pos_beats
+          -- end
+          -- cur = cur + (xtouch.transport.jog_wheel.delta.value * multiplier)
+          -- if cur < 0 then cur = 0 end
+          -- if cur >= len then cur = len end
+          -- print(cur, len)
+          -- pcall(function()
+          --   if renoise.song().transport.playing then
+          --     renoise.song().transport.playback_pos_beats = (cur + .1) / renoise.song().transport.lpb
+          --   else
+          --     renoise.song().transport.edit_pos_beats = (cur + .1) / renoise.song().transport.lpb
+          --   end
+          -- end)
         end,
         description = "Transport through entire song"
       },
